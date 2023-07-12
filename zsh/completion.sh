326820bd2800e1dd5d5c92691ab1ea55e3c5f486
zhengqiakai <zhengqiakai@x-cmd.com>
2023-07-12 06:28:33 +0000

    ...

--- zsh/completion.sh
+++ zsh/completion.sh
@@ -5,10 +5,11 @@ ___advise_completer_zsh_completions(){
 }
 
 ___x_cmd_advise_man_load__zsh_completions(){
+    [ -d "$___X_CMD_ADVISE_MAN_COMPLETIONS_FOLDER_SRC" ] || return 0
     local _line; while read -r _line;do
         compdef ___advise_completer_zsh_completions "${_line#*_}"
     done <<A
-$(x ls "$___X_CMD_ADVISE_MAN_COMPLETIONS_FOLDER_SRC")
+$( x ls "$___X_CMD_ADVISE_MAN_COMPLETIONS_FOLDER_SRC" 2>/dev/null )
 A
 }
 
